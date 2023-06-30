7f91eb943f0777cfcd6c9c61046e123f7eb97389
afsheen96 <18mit29260@gmail.com>
2023-06-30 22:29:57 +0100

    add object array

--- index.js
+++ index.js
@@ -13,21 +13,62 @@ element.addEventListener('click', menu);
   {
     id: '1',
     name: 'Tonic',
-    namedesk: 'Tonic',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    Descriptiondesk: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     longDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
     industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
     scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
     industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
     printer took a galley of type and scrambled it 1960s with the relea`,
-    featured: ['CANOPY', 'Back End Dev', 2023],
-    featureddesk: ['CANOPY', 'Back End Dev', 2023],
-    image: './First-image.jpg',
-    imageDesktop: './images/desktop/projectone.svg',
-    technologies: ['html', 'css', 'javaScript'],
-    technologiesdesk: ['html', 'css', 'javaScript', 'Ruby on rails;'],
+    featured: ['CANOPY', 'Back End Dev', 2015],
+    image: './resources/project1',
+    lang: ['html', 'css', 'javaScript'],
     live_version: 'https://afsheen96.github.io/Portfolio',
     source_link: 'https://github.com/Afsheen96/Portfolio',
-  }
- ]
+  },
+  {
+    id: '2',
+    name: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    longDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+    scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
+    industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+    printer took a galley of type and scrambled it 1960s with the relea`,
+    featured: ['CANOPY', 'Back End Dev', 2015],
+    image: './resources/project2',
+    lang: ['html', 'css', 'javaScript'],
+    live_version: 'https://afsheen96.github.io/Portfolio',
+    source_link: 'https://github.com/Afsheen96/Portfolio',
+  },
+  {
+    id: '3',
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    longDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+    scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
+    industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+    printer took a galley of type and scrambled it 1960s with the relea`,
+    featured: ['CANOPY', 'Back End Dev', 2015],
+    image: './resources/project3',
+    lang: ['html', 'css', 'javaScript'],
+    live_version: 'https://afsheen96.github.io/Portfolio',
+    source_link: 'https://github.com/Afsheen96/Portfolio',
+  },
+  {
+    id: '4',
+    name: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    longDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+    scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
+    industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+    printer took a galley of type and scrambled it 1960s with the relea`,
+    featured: ['CANOPY', 'Back End Dev', 2015],
+    image: './resources/project4',
+    lang: ['html', 'css', 'javaScript'],
+    live_version: 'https://afsheen96.github.io/Portfolio',
+    source_link: 'https://github.com/Afsheen96/Portfolio',
+  },
+ ];
+ 
