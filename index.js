0ed4836528fa10948467178e7482b00909670aec
afsheen96 <18mit29260@gmail.com>
2023-06-30 19:30:13 +0100

    create popup window section

--- index.js
+++ index.js
@@ -7,3 +7,27 @@ function menu() {
   }
 }
 element.addEventListener('click', menu);
+ 
+// project pop-up section
+ const projects =[
+  {
+    // id: '1',
+    // name: 'Tonic',
+    // namedesk: 'Tonic',
+    // shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    // shortDescriptiondesk: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    // longDescription: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+    // industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+    // scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting
+    // industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+    // printer took a galley of type and scrambled it 1960s with the relea`,
+    // featured: ['CANOPY', 'Back End Dev', 2023],
+    // featureddesk: ['CANOPY', 'Back End Dev', 2023],
+    // image: './First-image.jpg',
+    // imageDesktop: './images/desktop/projectone.svg',
+    // technologies: ['html', 'css', 'javaScript'],
+    // technologiesdesk: ['html', 'css', 'javaScript', 'Ruby on rails;'],
+    // live_version: 'https://ashnaali3255.github.io/Portfolio-2/',
+    // source_link: 'https://github.com/Ashnaali3255/Portfolio-2',
+  }
+ ]
